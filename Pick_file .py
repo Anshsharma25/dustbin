b4fb618da7fd404f8e195c2a9960e2575099af83
Ansh sharma <anshshama8nbi@gmail.com>
2025-01-03 16:10:55 +0530

    Add 2 models and update in Main file and after some time update the models

--- Pick_file .py
+++ Pick_file .py
@@ -10,14 +10,26 @@ drywet_model_metadata = {
     "model_path": "Main_DW.pt",
 }
 
-# Combine both models' metadata
+# Define metadata for cover/uncover classification model
+cover_uncover_model_metadata = {
+    "model_path": "cover_model.pt",
+}
+
+# Define metadata for polythene/non-polythene classification model
+polythene_nonpoly_model_metadata = {
+    "model_path": "poly_non_poly.pt",
+}
+
+# Combine all models' metadata
 all_models_metadata = {
     "garbage_model": garbage_model_metadata,
-    "drywet_model": drywet_model_metadata
+    "drywet_model": drywet_model_metadata,
+    "cover_uncover_model": cover_uncover_model_metadata,
+    "polythene_nonpoly_model": polythene_nonpoly_model_metadata
 }
 
 # Save to a pickle file
-pickle_file_path = "models_metadata.pkl"
+pickle_file_path = "models_pickle.pkl"
 with open(pickle_file_path, "wb") as file:
     pickle.dump(all_models_metadata, file)
 
